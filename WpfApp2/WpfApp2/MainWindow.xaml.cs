900518cf4e37582a5a6837c45d5210f009dd9b4c
Mohamed Medhat <blackmanmido21@gmail.com>
2019-12-03 21:36:20 +0200

    welcome window manuplation

--- WpfApp2/WpfApp2/MainWindow.xaml.cs
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace WpfApp2
 {
@@ -22,10 +11,26 @@ namespace WpfApp2
     {
         public MainWindow()
         {
+
             InitializeComponent();
-            Login l = new Login();
-            Register r = new Register();
-            r.Show();
+            StartCloseTimer();
+            void StartCloseTimer()
+            {
+                DispatcherTimer timer = new DispatcherTimer();
+                timer.Interval = TimeSpan.FromSeconds(3d);
+                timer.Tick += TimerTick;
+                timer.Start();
+            }
+            void TimerTick(object sender, EventArgs e)
+            {
+                DispatcherTimer timer = (DispatcherTimer)sender;
+                timer.Stop();
+                timer.Tick -= TimerTick;
+                dots.Text += ".";
+                Welcome w = new Welcome();
+                w.Show();
+                Close();
+            }
         }
     }
 }
